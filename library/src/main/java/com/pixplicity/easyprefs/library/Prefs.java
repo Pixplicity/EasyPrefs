7e9a8a578e1ec3c28768622b3aad01d20032e109
Mathijs Lagerberg <mathijs@pixplicity.com>
2015-11-25 19:15:45 +0100

    Support for ordered Sets
    [add] putOrderedStringSet and getOrderedStringSet
    [mod] added and fixed comments

--- library/src/main/java/com/pixplicity/easyprefs/library/Prefs.java
+++ library/src/main/java/com/pixplicity/easyprefs/library/Prefs.java
@@ -12,6 +12,7 @@ import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
+@SuppressWarnings("unused")
 public final class Prefs {
 
     private static final String DEFAULT_SUFFIX = "_preferences";
@@ -21,8 +22,9 @@ public final class Prefs {
     /**
      * Initialize the Prefs helper class to keep a reference to the SharedPreference for this
      * application the SharedPreference will use the package name of the application as the Key.
-     * <p/>
+     * <p>
      * This method is deprecated please us the new builder.
+     * </p>
      *
      * @param context the Application context.
      */
@@ -31,25 +33,22 @@ public final class Prefs {
         new Builder().setContext(context).build();
     }
 
-    /**
-     * @hide
-     */
     private static void initPrefs(Context context, String prefsName, int mode) {
         mPrefs = context.getSharedPreferences(prefsName, mode);
     }
 
     /**
-     * Returns an instance of the shared preference for this app.
+     * Returns the underlying SharedPreference instance
      *
-     * @return an Instance of the SharedPreference
-     * @throws RuntimeException if sharedpreference instance has not been instatiated yet.
+     * @return an instance of the SharedPreference
+     * @throws RuntimeException if SharedPreference instance has not been instantiated yet.
      */
     public static SharedPreferences getPreferences() {
         if (mPrefs != null) {
             return mPrefs;
         }
         throw new RuntimeException(
-                "Prefs class not correctly instantiated please call Builder.setContext().build(); in the Application class onCreate.");
+                "Prefs class not correctly instantiated. Please call Builder.setContext().build() in the Application class onCreate.");
     }
 
     /**
@@ -62,11 +61,13 @@ public final class Prefs {
     }
 
     /**
+     * Retrieves a stored int value.
+     *
      * @param key      The name of the preference to retrieve.
      * @param defValue Value to return if this preference does not exist.
-     * @return Returns the preference value if it exists, or defValue.  Throws
-     * ClassCastException if there is a preference with this name that is not
-     * an int.
+     * @return Returns the preference value if it exists, or defValue.
+     * @throws ClassCastException if there is a preference with this name that is not
+     *                            an int.
      * @see android.content.SharedPreferences#getInt(String, int)
      */
     public static int getInt(final String key, final int defValue) {
@@ -74,11 +75,12 @@ public final class Prefs {
     }
 
     /**
+     * Retrieves a stored boolean value.
+     *
      * @param key      The name of the preference to retrieve.
      * @param defValue Value to return if this preference does not exist.
-     * @return Returns the preference value if it exists, or defValue.  Throws
-     * ClassCastException if there is a preference with this name that is not
-     * a boolean.
+     * @return Returns the preference value if it exists, or defValue.
+     * @throws ClassCastException if there is a preference with this name that is not a boolean.
      * @see android.content.SharedPreferences#getBoolean(String, boolean)
      */
     public static boolean getBoolean(final String key, final boolean defValue) {
@@ -86,11 +88,12 @@ public final class Prefs {
     }
 
     /**
+     * Retrieves a stored long value.
+     *
      * @param key      The name of the preference to retrieve.
      * @param defValue Value to return if this preference does not exist.
-     * @return Returns the preference value if it exists, or defValue.  Throws
-     * ClassCastException if there is a preference with this name that is not
-     * a long.
+     * @return Returns the preference value if it exists, or defValue.
+     * @throws ClassCastException if there is a preference with this name that is not a long.
      * @see android.content.SharedPreferences#getLong(String, long)
      */
     public static long getLong(final String key, final long defValue) {
@@ -102,9 +105,8 @@ public final class Prefs {
      *
      * @param key      The name of the preference to retrieve.
      * @param defValue the double Value to return if this preference does not exist.
-     * @return Returns the preference value if it exists, or defValue.  Throws
-     * ClassCastException if there is a preference with this name that is not
-     * a long.
+     * @return Returns the preference value if it exists, or defValue.
+     * @throws ClassCastException if there is a preference with this name that is not a long.
      * @see android.content.SharedPreferences#getLong(String, long)
      */
     public static double getDouble(final String key, final double defValue) {
@@ -112,11 +114,12 @@ public final class Prefs {
     }
 
     /**
+     * Retrieves a stored float value.
+     *
      * @param key      The name of the preference to retrieve.
      * @param defValue Value to return if this preference does not exist.
-     * @return Returns the preference value if it exists, or defValue.  Throws
-     * ClassCastException if there is a preference with this name that is not
-     * a float.
+     * @return Returns the preference value if it exists, or defValue.
+     * @throws ClassCastException if there is a preference with this name that is not a float.
      * @see android.content.SharedPreferences#getFloat(String, float)
      */
     public static float getFloat(final String key, final float defValue) {
@@ -124,11 +127,12 @@ public final class Prefs {
     }
 
     /**
+     * Retrieves a stored String value.
+     *
      * @param key      The name of the preference to retrieve.
      * @param defValue Value to return if this preference does not exist.
-     * @return Returns the preference value if it exists, or defValue.  Throws
-     * ClassCastException if there is a preference with this name that is not
-     * a String.
+     * @return Returns the preference value if it exists, or defValue.
+     * @throws ClassCastException if there is a preference with this name that is not a String.
      * @see android.content.SharedPreferences#getString(String, String)
      */
     public static String getString(final String key, final String defValue) {
@@ -136,12 +140,18 @@ public final class Prefs {
     }
 
     /**
+     * Retrieves a Set of Strings as stored by {@link #putStringSet(String, Set)}. On Honeycomb and
+     * later this will call the native implementation in SharedPreferences, on older SDKs this will
+     * call {@link #getOrderedStringSet(String, Set)}.
+     * <strong>Note that the native implementation of {@link SharedPreferences#getStringSet(String,
+     * Set)} does not reliably preserve the order of the Strings in the Set.</strong>
+     *
      * @param key      The name of the preference to retrieve.
      * @param defValue Value to return if this preference does not exist.
-     * @return Returns the preference values if they exist, or defValues.
-     * Throws ClassCastException if there is a preference with this name
-     * that is not a Set.
+     * @return Returns the preference values if they exist, or defValues otherwise.
+     * @throws ClassCastException if there is a preference with this name that is not a Set.
      * @see android.content.SharedPreferences#getStringSet(String, java.util.Set)
+     * @see #getOrderedStringSet(String, Set)
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static Set<String> getStringSet(final String key, final Set<String> defValue) {
@@ -149,22 +159,41 @@ public final class Prefs {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             return prefs.getStringSet(key, defValue);
         } else {
-            if (prefs.contains(key + LENGTH)) {
-                LinkedHashSet<String> set = new LinkedHashSet<>();
-                // Workaround for pre-HC's lack of StringSets
-                int stringSetLength = prefs.getInt(key + LENGTH, -1);
-                if (stringSetLength >= 0) {
-                    for (int i = 0; i < stringSetLength; i++) {
-                        set.add(prefs.getString(key + "[" + i + "]", null));
-                    }
+            // Workaround for pre-HC's missing getStringSet
+            return getOrderedStringSet(key, defValue);
+        }
+    }
+
+    /**
+     * Retrieves a Set of Strings as stored by {@link #putOrderedStringSet(String, Set)},
+     * preserving the original order. Note that this implementation is heavier than the native
+     * {@link #getStringSet(String, Set)} method (which does not guarantee to preserve order).
+     *
+     * @param key      The name of the preference to retrieve.
+     * @param defValue Value to return if this preference does not exist.
+     * @return Returns the preference value if it exists, or defValues otherwise.
+     * @throws ClassCastException if there is a preference with this name that is not a Set of
+     *                            Strings.
+     * @see #getStringSet(String, Set)
+     */
+    public static Set<String> getOrderedStringSet(String key, final Set<String> defValue) {
+        SharedPreferences prefs = getPreferences();
+        if (prefs.contains(key + LENGTH)) {
+            LinkedHashSet<String> set = new LinkedHashSet<>();
+            int stringSetLength = prefs.getInt(key + LENGTH, -1);
+            if (stringSetLength >= 0) {
+                for (int i = 0; i < stringSetLength; i++) {
+                    set.add(prefs.getString(key + "[" + i + "]", null));
                 }
-                return set;
             }
+            return set;
         }
         return defValue;
     }
 
     /**
+     * Stores a long value.
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
      * @see android.content.SharedPreferences.Editor#putLong(String, long)
@@ -180,6 +209,8 @@ public final class Prefs {
     }
 
     /**
+     * Stores an integer value.
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
      * @see android.content.SharedPreferences.Editor#putInt(String, int)
@@ -195,7 +226,7 @@ public final class Prefs {
     }
 
     /**
-     * Saves the double as a long raw bits inside the preferences.
+     * Stores a double value as a long raw bits value.
      *
      * @param key   The name of the preference to modify.
      * @param value The double value to be save in the preferences.
@@ -212,6 +243,8 @@ public final class Prefs {
     }
 
     /**
+     * Stores a float value.
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
      * @see android.content.SharedPreferences.Editor#putFloat(String, float)
@@ -227,6 +260,8 @@ public final class Prefs {
     }
 
     /**
+     * Stores a boolean value.
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
      * @see android.content.SharedPreferences.Editor#putBoolean(String, boolean)
@@ -242,6 +277,8 @@ public final class Prefs {
     }
 
     /**
+     * Stores a String value.
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
      * @see android.content.SharedPreferences.Editor#putString(String, String)
@@ -257,32 +294,61 @@ public final class Prefs {
     }
 
     /**
+     * Stores a Set of Strings. On Honeycomb and later this will call the native implementation in
+     * SharedPreferences.Editor, on older SDKs this will call {@link #putOrderedStringSet(String,
+     * Set)}.
+     * <strong>Note that the native implementation of {@link Editor#putStringSet(String,
+     * Set)} does not reliably preserve the order of the Strings in the Set.</strong>
+     *
      * @param key   The name of the preference to modify.
      * @param value The new value for the preference.
      * @see android.content.SharedPreferences.Editor#putStringSet(String, java.util.Set)
+     * @see #putOrderedStringSet(String, Set)
      */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static void putStringSet(final String key, final Set<String> value) {
-        final Editor editor = getPreferences().edit();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            final Editor editor = getPreferences().edit();
             editor.putStringSet(key, value);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD) {
+                editor.commit();
+            } else {
+                editor.apply();
+            }
         } else {
             // Workaround for pre-HC's lack of StringSets
-            int stringSetLength = 0;
-            if (mPrefs.contains(key + LENGTH)) {
-                // First read what the value was
-                stringSetLength = mPrefs.getInt(key + LENGTH, -1);
-            }
-            editor.putInt(key + LENGTH, value.size());
-            int i = 0;
-            for (String aValue : value) {
-                editor.putString(key + "[" + i + "]", aValue);
-                i++;
-            }
-            for (; i < stringSetLength; i++) {
-                // Remove any remaining values
-                editor.remove(key + "[" + i + "]");
-            }
+            putOrderedStringSet(key, value);
+        }
+    }
+
+    /**
+     * Stores a Set of Strings, preserving the order.
+     * Note that this method is heavier that the native implementation {@link #putStringSet(String,
+     * Set)} (which does not reliably preserve the order of the Set). To preserve the order of the
+     * items in the Set, the Set implementation must be one that as an iterator with predictable
+     * order, such as {@link LinkedHashSet}.
+     *
+     * @param key   The name of the preference to modify.
+     * @param value The new value for the preference.
+     * @see #putStringSet(String, Set)
+     * @see #getOrderedStringSet(String, Set)
+     */
+    private static void putOrderedStringSet(String key, Set<String> value) {
+        final Editor editor = getPreferences().edit();
+        int stringSetLength = 0;
+        if (mPrefs.contains(key + LENGTH)) {
+            // First read what the value was
+            stringSetLength = mPrefs.getInt(key + LENGTH, -1);
+        }
+        editor.putInt(key + LENGTH, value.size());
+        int i = 0;
+        for (String aValue : value) {
+            editor.putString(key + "[" + i + "]", aValue);
+            i++;
+        }
+        for (; i < stringSetLength; i++) {
+            // Remove any remaining values
+            editor.remove(key + "[" + i + "]");
         }
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.GINGERBREAD) {
             editor.commit();
@@ -292,6 +358,8 @@ public final class Prefs {
     }
 
     /**
+     * Removes a preference value.
+     *
      * @param key The name of the preference to remove.
      * @see android.content.SharedPreferences.Editor#remove(String)
      */
@@ -318,8 +386,10 @@ public final class Prefs {
     }
 
     /**
+     * Checks if a value is stored for the given key.
+     *
      * @param key The name of the preference to check.
-     * @return true if preference contains this key value.
+     * @return {@code true} if the storage contains this key value, {@code false} otherwise.
      * @see android.content.SharedPreferences#contains(String)
      */
     public static boolean contains(final String key) {
@@ -327,6 +397,8 @@ public final class Prefs {
     }
 
     /**
+     * Removed all the stored keys and values.
+     *
      * @return the {@link Editor} for chaining. The changes have already been committed/applied
      * through the execution of this method.
      * @see android.content.SharedPreferences.Editor#clear()
@@ -341,6 +413,11 @@ public final class Prefs {
         return editor;
     }
 
+    /**
+     * Returns the Editor of the underlying SharedPreferences instance.
+     *
+     * @return An Editor
+     */
     public static Editor edit() {
         return getPreferences().edit();
     }
@@ -357,10 +434,10 @@ public final class Prefs {
         private boolean mUseDefault = false;
 
         /**
-         * Set the filename of the sharedprefence instance  usually this is the applications
-         * packagename.xml but for migration purposes or customization.
+         * Set the filename of the SharedPreference instance. Usually this is the application's
+         * packagename.xml but it can be modified for migration purposes or customization.
          *
-         * @param prefsName the filename used for the sharedpreference
+         * @param prefsName the filename used for the SharedPreference
          * @return the {@link com.pixplicity.easyprefs.library.Prefs.Builder} object.
          */
         public Builder setPrefsName(final String prefsName) {
@@ -369,7 +446,7 @@ public final class Prefs {
         }
 
         /**
-         * Set the context used to instantiate the sharedpreferences
+         * Set the Context used to instantiate the SharedPreferences
          *
          * @param context the application context
          * @return the {@link com.pixplicity.easyprefs.library.Prefs.Builder} object.
@@ -380,7 +457,7 @@ public final class Prefs {
         }
 
         /**
-         * Set the mode of the sharedpreference instance.
+         * Set the mode of the SharedPreference instance.
          *
          * @param mode Operating mode.  Use 0 or {@link Context#MODE_PRIVATE} for the
          *             default operation, {@link Context#MODE_WORLD_READABLE}
@@ -391,18 +468,19 @@ public final class Prefs {
             if (mode == ContextWrapper.MODE_PRIVATE || mode == ContextWrapper.MODE_WORLD_READABLE || mode == ContextWrapper.MODE_WORLD_WRITEABLE || mode == ContextWrapper.MODE_MULTI_PROCESS) {
                 mMode = mode;
             } else {
-                throw new RuntimeException("The mode in the sharedpreference can only be set too ContextWrapper.MODE_PRIVATE, ContextWrapper.MODE_WORLD_READABLE, ContextWrapper.MODE_WORLD_WRITEABLE or ContextWrapper.MODE_MULTI_PROCESS");
+                throw new RuntimeException("The mode in the SharedPreference can only be set too ContextWrapper.MODE_PRIVATE, ContextWrapper.MODE_WORLD_READABLE, ContextWrapper.MODE_WORLD_WRITEABLE or ContextWrapper.MODE_MULTI_PROCESS");
             }
 
             return this;
         }
 
         /**
-         * Set the default sharedpreference file name. Often the package name of the application is
+         * Set the default SharedPreference file name. Often the package name of the application is
          * used, but if the {@link android.preference.PreferenceActivity} or {@link
-         * android.preference.PreferenceFragment} is used android append that with _preference.
+         * android.preference.PreferenceFragment} is used the system will append that with
+         * _preference.
          *
-         * @param defaultSharedPreference true if default sharedpreference name should used.
+         * @param defaultSharedPreference true if default SharedPreference name should used.
          * @return the {@link com.pixplicity.easyprefs.library.Prefs.Builder} object.
          */
         public Builder setUseDefaultSharedPreference(boolean defaultSharedPreference) {
@@ -411,9 +489,9 @@ public final class Prefs {
         }
 
         /**
-         * Initialize the sharedpreference instance to used in the application.
+         * Initialize the SharedPreference instance to used in the application.
          *
-         * @throws RuntimeException if context has not been set.
+         * @throws RuntimeException if Context has not been set.
          */
         public void build() {
             if (mContext == null) {
